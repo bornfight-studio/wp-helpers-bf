b40395c56accfa6036bdb271fe9d4e6500a069be
josip <jmucak@outlook.com>
2020-05-10 20:50:11 +0200

    changed post data provider, added terms data provider

--- src/PostDataProvider.php
+++ src/PostDataProvider.php
@@ -8,28 +8,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 class PostDataProvider {
-	public function get_post_data( string $post_type, int $posts_per_page = - 1 ): array {
+	public function get_post_data( string $post_type = 'post', int $posts_per_page = - 1, int $paged = null ): array {
+
 		$args = [
 			'post_type'      => $post_type,
 			'posts_per_page' => $posts_per_page,
 			'post_status'    => 'publish'
 		];
 
+		if ( ! empty( $paged ) ) {
+			$args['paged'] = $paged;
+		}
+
 		$query = new \WP_Query( $args );
 
 		$response['max_pages'] = $query->max_num_pages;
-
-		$response['posts'] = $query->get_posts();
+		$response['posts']     = $query->get_posts();
 
 		return $response;
-	}
-
-	public function get_taxonomy( string $taxonomy, bool $hide_empty = false ) {
-		$terms = get_terms( [
-			'taxonomy'   => $taxonomy,
-			'hide_empty' => $hide_empty
-		] );
-
-		return $terms;
 	}
 }
