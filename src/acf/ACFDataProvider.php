640000fbc77e9628e5b785f47c24d192d6083570
josip <josip.mucak@bornfight.com>
2022-02-27 14:29:43 +0100

    update ACFDataProvider class update composer json

--- src/acf/ACFDataProvider.php
+++ src/acf/ACFDataProvider.php
@@ -6,128 +6,114 @@ namespace bornfight\wpHelpers\acf;
  * Class ACFDataProvider
  * @package app\helpers
  */
-class ACFDataProvider
-{
-    /**
-     *
-     */
-    const OPTION = 'option';
-    /**
-     * @var null
-     */
-    private static $instance = null;
+class ACFDataProvider {
+	const OPTION = 'option';
 
-    /**
-     * @var string
-     */
-    private $prefix = '';
+	private static ?ACFDataProvider $instance = null;
 
-    /**
-     * @var array
-     */
-    private $fields = [];
+	private string $prefix = '';
 
-    /**
-     * DataProvider constructor.
-     */
-    private function __construct()
-    {
-    }
+	private array $fields = [];
 
-    /**
-     * @return ACFDataProvider|null
-     */
-    public static function get_instance()
-    {
-        if (self::$instance === null) {
-            self::$instance = new ACFDataProvider();
-        }
+	private function __construct() {
+	}
 
-        return self::$instance;
-    }
+	public static function get_instance(): ?ACFDataProvider {
+		if ( self::$instance === null ) {
+			self::$instance = new ACFDataProvider();
+		}
+
+		return self::$instance;
+	}
 
 
-    /**
-     * @param $name
-     * @param bool $prefixed
-     * @return bool|mixed|null
-     */
-    public function get_option_field($name, $prefixed = true)
-    {
-        return $this->get_field($name, self::OPTION, $prefixed);
-    }
+	/**
+	 * @param string $name
+	 * @param bool $prefixed
+	 *
+	 * @return bool|mixed|null
+	 */
+	public function get_option_field( string $name, bool $prefixed = true ) {
+		return $this->get_field( $name, self::OPTION, $prefixed );
+	}
 
-    /**
-     * @param $name
-     * @param bool $post_id
-     * @param bool $prefixed
-     *
-     * @return bool|mixed|null
-     */
-    public function get_field($name, $post_id = false, $prefixed = true)
-    {
-        $post_id = $post_id !== false ? $post_id : get_the_ID();
-        $key = ($prefixed ? $this->prefix : '' ) . $name;
+	/**
+	 * @param string $name
+	 * @param int|string $post_id
+	 * @param bool $prefixed
+	 *
+	 * @return bool|mixed|null
+	 */
+	public function get_field( string $name, $post_id = '', bool $prefixed = true ) {
+		$post_id = ! empty( $post_id ) ? $post_id : get_the_ID();
+		$key     = ( $prefixed ? $this->prefix : '' ) . $name;
 
-        $cache_key = 'field_' . $key . '_' . $post_id;
-        $fields_cache_key = 'fields_' . $post_id;
+		$cache_key        = 'field_' . $key . '_' . $post_id;
+		$fields_cache_key = 'fields_' . $post_id;
 
-        if (isset($this->fields[$cache_key])) {
-            return $this->fields[$cache_key];
-        } elseif (isset($this->fields[$fields_cache_key]) && isset($this->fields[$fields_cache_key][$key])) {
-            return $this->fields[$fields_cache_key][$key];
-        }
+		if ( isset( $this->fields[ $cache_key ] ) ) {
+			return $this->fields[ $cache_key ];
+		} elseif ( isset( $this->fields[ $fields_cache_key ] ) && isset( $this->fields[ $fields_cache_key ][ $key ] ) ) {
+			return $this->fields[ $fields_cache_key ][ $key ];
+		}
 
-        $this->fields[$cache_key] = get_field($key, $post_id);
-        return $this->fields[$cache_key];
-    }
+		$this->fields[ $cache_key ] = get_field( $key, $post_id );
 
-    /**
-     * @param string $prefix
-     * @return $this
-     */
-    public function set_prefix($prefix)
-    {
-        $this->prefix = $prefix;
-        return $this;
-    }
+		return $this->fields[ $cache_key ];
+	}
 
-    /**
-     * @return $this
-     */
-    public function clear_prefix()
-    {
-        $this->prefix = '';
-        return $this;
-    }
+	/**
+	 * @param string $prefix
+	 *
+	 * @return $this
+	 */
+	public function set_prefix( string $prefix ): ACFDataProvider {
+		$this->prefix = $prefix;
 
-    /**
-     * @param bool $post_id
-     *
-     * @return array|bool
-     */
-    public function get_fields($post_id = false)
-    {
-        $post_id = $post_id !== false ? $post_id : get_the_ID();
-        $key = 'fields_' . $post_id;
-        if (isset($this->fields[$key])) {
-            return $this->fields[$key];
-        }
-        $this->fields[$key] = get_fields($post_id);
+		return $this;
+	}
 
-        return $this->fields[$key];
-    }
+	/**
+	 * @return $this
+	 */
+	public function clear_prefix(): ACFDataProvider {
+		$this->prefix = '';
 
-    public function get_user_field($name, $user_id = null, $prefixed = true)
-    {
-        if ($user_id === null && is_single()) {
-            $user_id = get_the_author_meta('ID');
-        }
+		return $this;
+	}
 
-        if ($user_id) {
-            return $this->get_field($name, 'user_' . $user_id, $prefixed);
-        }
+	/**
+	 * @param int|string $post_id
+	 *
+	 * @return array|bool
+	 */
+	public function get_fields( $post_id = '' ) {
+		$post_id = ! empty( $post_id ) ? $post_id : get_the_ID();
+		$key     = 'fields_' . $post_id;
+		if ( isset( $this->fields[ $key ] ) ) {
+			return $this->fields[ $key ];
+		}
+		$this->fields[ $key ] = get_fields( $post_id );
 
-        return '';
-    }
+		return $this->fields[ $key ];
+	}
+
+	/**
+	 * @param string $name
+	 * @param null $user_id
+	 * @param bool $prefixed
+	 *
+	 * @return bool|mixed|string|null
+	 */
+	public function get_user_field( string $name, $user_id = null, bool $prefixed = true ) {
+		if ( $user_id === null && is_single() ) {
+			$user_id = get_the_author_meta( 'ID' );
+		}
+
+		if ( $user_id ) {
+			return $this->get_field( $name, 'user_' . $user_id, $prefixed );
+		}
+
+		return '';
+	}
 }
