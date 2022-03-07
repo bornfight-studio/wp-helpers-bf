1473ef48e59b1a1528cba71e22536ada5b8f4830
josip <josip.mucak@bornfight.com>
2022-03-07 13:16:10 +0100

    update optimization classes

--- src/optimization/BaseWPDefaultsOptimization.php
+++ src/optimization/BaseWPDefaultsOptimization.php
@@ -10,37 +10,37 @@ use bornfight\wpHelpers\optimization\defaults\WPJsDependencies;
 use bornfight\wpHelpers\optimization\defaults\WPPosts;
 
 abstract class BaseWPDefaultsOptimization {
-	public function deactivate_gutenberg_scripts( array $settings = array() ): void {
+	protected function deactivate_gutenberg_scripts( array $settings = array() ): void {
 		$gutenberg_scripts = new GutenbergScripts();
 		$gutenberg_scripts->deactivate( $settings );
 	}
 
-	public function deactivate_wp_comments( array $settings = array() ): void {
+	protected function deactivate_wp_comments( array $settings = array() ): void {
 		$wp_comments = new WPComments();
 		$wp_comments->deactivate( $settings );
 	}
 
-	public function deactivate_wp_embeds( array $settings = array() ): void {
+	protected function deactivate_wp_embeds( array $settings = array() ): void {
 		$wp_embeds = new WPEmbeds();
 		$wp_embeds->deactivate( $settings );
 	}
 
-	public function deactivate_wp_emoji( array $settings = array() ): void {
+	protected function deactivate_wp_emoji( array $settings = array() ): void {
 		$wp_emoji = new WPEmoji();
 		$wp_emoji->deactivate( $settings );
 	}
 
-	public function deactivate_wp_js_dependencies( array $settings = array() ): void {
+	protected function deactivate_wp_js_dependencies( array $settings = array() ): void {
 		$wp_js_dependencies = new WPJsDependencies();
 		$wp_js_dependencies->deactivate( $settings );
 	}
 
-	public function deactivate_wp_posts( array $settings = array() ): void {
+	protected function deactivate_wp_posts( array $settings = array() ): void {
 		$wp_posts = new WPPosts();
 		$wp_posts->deactivate( $settings );
 	}
 
-	public function add_jquery_cdn(): void {
+	protected function add_jquery_cdn(): void {
 		if ( is_admin() ) {
 			return;
 		}
--- src/optimization/BaseWPPluginsOptimization.php
+++ src/optimization/BaseWPPluginsOptimization.php
@@ -6,12 +6,12 @@ use bornfight\wpHelpers\optimization\plugins\ACFEOptions;
 use bornfight\wpHelpers\optimization\plugins\CF7Options;
 
 abstract class BaseWPPluginsOptimization {
-	public function deactivate_acfe_options( array $settings = array() ): void {
+	protected function deactivate_acfe_options( array $settings = array() ): void {
 		$acfe_options = new ACFEOptions();
 		$acfe_options->deactivate( $settings );
 	}
 
-	public function deactivate_cf7_options( array $settings = array() ): void {
+	protected function deactivate_cf7_options( array $settings = array() ): void {
 		$cf7_options = new CF7Options();
 		$cf7_options->deactivate( $settings );
 	}
